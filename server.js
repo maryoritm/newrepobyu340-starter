e6caabe59d2c8d968951c392bb53758faa56bd94
maryoritm <maryor.trrs@gmail.com>
2025-07-04 17:32:58 -0600

    W01

--- server.js
+++ server.js
@@ -6,14 +6,31 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs"); // Configura EJS como motor de plantillas
+app.use(expressLayouts); // Habilita el uso de layouts
+app.set("layout", "./layouts/layout"); // Ruta del layout base (dentro de views/)
+
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
+
+
+// Index route
+app.get('/', function(req, res) {
+  res.render('index', {title: 'Home' // Pasa el título al partial 'head.ejs'
+});
+});
+
 
 /* ***********************
  * Local Server Information
