324d137937d1ad7b7d1fb03cd958a2aafa3e93e9
maryoritm <maryor.trrs@gmail.com>
2025-07-18 12:08:27 -0600

    w03

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(static)
 
 // Index route
 // Index route
-app.get("/", baseController.buildHome);
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
@@ -45,9 +45,10 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
 })
